228584d9db0ae4832546384d2c404023e4f57dd2
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-08-11 15:51:53 +0530

    updated

---
+++ index.js
@@ -0,0 +1,48 @@
+const button = document.querySelector("#button");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const noOfNotes = document.querySelector(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
+
+
+function calculateAmount(amountToBeReturened){
+
+    for(var i= 0; i < availableNotes.lenght ; i++){
+
+        const notes = Math.trunc(amountToBeReturened / availableNotes[i] );
+        amountToBeReturened = amountToBeReturened % availableNotes[i];
+        noOfNotes.innerText = notes;
+    }
+};
+
+
+function showMessage(msg){
+    message.innerText = msg;
+
+};
+
+
+button.addEventListener("click",  function clickHandler(){
+
+    if(billAmount.value > 0){
+
+        if(cashGiven.value > billAmount.value){
+            const amountToBeReturened = cashGiven.value - billAmount.value;
+            calculateAmount(amountToBeReturened);
+        }
+        else{
+            showMessage("come and wash the dishes");
+        }
+    }
+    else{
+        showMessage("Invalid Amount")
+    }
+    
+
+});
+
+
+
+
+
