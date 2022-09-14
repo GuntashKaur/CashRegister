c14deee29a8e99d97bfa3902f3924df0ac803f0c
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-09-14 06:28:05 +0530

    fixing

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const button = document.querySelector("#button");
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const output = document.querySelector("#outputEl");
 
-const availableNotes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-const message = " ";
+const availableNotes = [ 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+// const message = " ";
 
 // console.log("message",message)
 function calculateAmount(amountToBeReturened){
 
-    for(var i= 0; i < availableNotes.length ; i++){
+    for(var i=0; i < availableNotes.length; i++){
 
         const notes = Math.trunc(amountToBeReturened / availableNotes[i] );
         amountToBeReturened = amountToBeReturened % availableNotes[i];
@@ -18,12 +19,12 @@ function calculateAmount(amountToBeReturened){
     }
 };
 
-console.log("message",message);
+// console.log("message",message);
 
-function showMessage(msg){
-    message.innerText = msg;
+// function showMessage(msg){
+//     message.innerText = msg;
 
-};
+// };
 
 
 button.addEventListener("click",  function clickHandler(){
@@ -31,19 +32,23 @@ button.addEventListener("click",  function clickHandler(){
     if(billAmount.value > 0){
 // console.log("if")
         if(cashGiven.value >= billAmount.value){
-            console.log("if")
+            // console.log("if")
+            // console.log("cash given is greater");
             const amountToBeReturened = cashGiven.value - billAmount.value;
             calculateAmount(amountToBeReturened);
+            
         }
         else{
-            console.log("else")
-            showMessage("come and wash the dishes");
+            // console.log("else")
+            // showMessage("come and wash the dishes");
+            output.innerText = "come and wash the dishes"
         }
     }
     
     else{
         
-        showMessage("Invalid Amount")
+        // showMessage("Invalid Amount")
+        output.innerText = "Invalid Amount"
     }
     
 
